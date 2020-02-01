e3774aff2d4f525fcd238913395601004806060d
Rigamorphius <43024627+Rigamorphius@users.noreply.github.com>
2020-01-31 18:25:44 -0600

    Initial Project Files

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int Movespeed = 0;
+
+    float horizontal;
+    float vertical;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(horizontal * Movespeed, vertical * Movespeed, 0.0f);
+        transform.position = transform.position + movement * Time.deltaTime;
+    }
+}
