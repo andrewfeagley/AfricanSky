92e89f0b3b08364ae1521d6f7a90b05e0c975b79
Rigamorphius <43024627+Rigamorphius@users.noreply.github.com>
2020-02-07 13:57:09 -0600

    Animations
    Added Animations as well as implemented Jump,Attack,Walk and Idle animations

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public bool inSight;
+    public GameObject player;
+    private Rigidbody2D rigidBody;
+    private float movementSpeed;
+    public float walkMoveSpeed = 2;
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        rigidBody = GetComponent<Rigidbody2D>();
+        movementSpeed = walkMoveSpeed;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject == player) {
+            inSight = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject == player) {
+            inSight = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float walkMoveSpeed;
+    public float attackMovementSpeed;
+    public float xMin, xMax, yMin, yMax;
+
+    AnimatorStateInfo currentStateInfo;
+    private float movementSpeed;
+    private Rigidbody2D rigidBody;
+    Animator anim;
+    private bool facingRight;
+    private bool facingLeft;
+
+    static int currentState;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        movementSpeed = walkMoveSpeed;
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal,moveVertical);
+
+        rigidBody.velocity = movement * movementSpeed;
+        rigidBody.position = new Vector2(Mathf.Clamp(rigidBody.position.x, xMin, xMax), Mathf.Clamp(rigidBody.position.y, yMin, yMax));
+
+        if (moveHorizontal < 0 && !facingRight) {
+            Flip();
+        }
+        else if (moveHorizontal > 0 && facingRight)
+        {
+            Flip();
+        }
+
+        if (Input.GetButton("Fire1")) {
+            anim.Play("Attack 1");
+        } 
+
+        if (Input.GetButton("Jump"))
+        {
+                anim.Play("Jump");
+        }
+
+
+        anim.SetFloat("moveSpeed", rigidBody.velocity.sqrMagnitude);
+    }
+
+    private void Flip()
+    {
+            facingRight = !facingRight;
+            Vector2 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+
+    }    
+}
