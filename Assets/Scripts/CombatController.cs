a47db9dbf19266c2c2d555f950a61c45cd82bafb
Charlie Phillips <hi.charliephillips@gmail.com>
2020-02-14 18:26:52 -0600

    created combat controller script

---
+++ Assets/Scripts/CombatController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatController : MonoBehaviour
+{
+    [SerializeField] int health;
+    [SerializeField] float damage;
+    [SerializeField] 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
