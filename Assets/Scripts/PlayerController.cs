a284c8b73d86d57f7e35d47ad0b4c5b274a23c1c
Rigamorphius <43024627+Rigamorphius@users.noreply.github.com>
2020-02-20 22:14:22 -0600

    Revert "Added Hitboxes"
    This reverts commit 6f72bee798ecb597325bb75cea7a6a00b17f7a32.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,9 +13,7 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D rigidBody;
     Animator anim;
-    public GameObject attackBox1, attackBox2, attackBox3;
-    public Sprite attack1Hitframe, attack2Hitframe, attack3Hitframe;
-    SpriteRenderer currentSprite;
+
     //Position in which the Player is facing.
     private bool facingRight;
     private bool facingLeft;
@@ -38,7 +36,6 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        currentSprite = GetComponent<SpriteRenderer>();
         rigidBody = GetComponent<Rigidbody2D>();
         movementSpeed = walkMoveSpeed;
         anim = GetComponent<Animator>();
@@ -122,47 +119,16 @@ public class PlayerController : MonoBehaviour
             anim.Play("Attack 1");
         }
 
-        if (Input.GetButton("Fire2"))
-        {
-            anim.Play("Attack 2");
-        }
-
-        if (Input.GetButton("Fire3"))
-        {
-            anim.Play("Attack 3");
-        }
-
-        //Checks to see if the attack frame is equal to the current in-game frame and runs code.
-        if (attack1Hitframe == currentSprite.sprite)
-        {
-            attackBox1.gameObject.SetActive(true);
-        }
-        else {
-            attackBox1.gameObject.SetActive(false);
-        }
-
-        if (attack2Hitframe == currentSprite.sprite)
-        {
-            attackBox2.gameObject.SetActive(true);
-        }
-        else
-        {
-            attackBox2.gameObject.SetActive(false);
-        }
-
-        if (attack3Hitframe == currentSprite.sprite)
-        {
-            attackBox3.gameObject.SetActive(true);
-        }
-        else
-        {
-            attackBox3.gameObject.SetActive(false);
-        }
-
         //Plays jump animation
         if (Input.GetButton("Jump"))
         {
             anim.Play("Jump");
+        }
+
+        if (Input.GetButton("Fire2"))
+        {
+            anim.Play("Hurt");
+
         }
 
         //Plays movement animation
