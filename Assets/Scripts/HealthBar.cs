5e9219ecc2b6689a23cb743d126f898e988524df
andrewfeagley <andrew.feagley@loop.colum.edu>
2020-05-01 17:43:20 -0500

    UI Working for player healthbar and Lives
    Fixes HealthBar.cs to work correctly

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Slider))]
 public class HealthBar : MonoBehaviour
 {
-    [SerializeField] IHaveHealth haveHealth; //should be set in the inspector to either the player or an enemy
+    [SerializeField] Player haveHealth; //should be set in the inspector to either the player or an enemy
     Image Health;
     float Fill;
-    [SerializeField] Slider slider;
+    [SerializeField] Image image;
+    Player p;
 
     // Start is called before the first frame update
     void Start()
@@ -19,16 +19,16 @@ public class HealthBar : MonoBehaviour
         //Looks for an event of OnHealthChanged in the IHaveHealth variable
         haveHealth.OnHealthChanged += HaveHealth_OnHealthChanged;
 
-        if (slider == null)
-            slider = GetComponent<Slider>();
+
+        if (image == null)
+            image = GetComponent<Image>();
 
         //Changes the value of the slider to the value of the IHaveHealth variable
-        slider.maxValue = haveHealth.Health;
-        slider.value = haveHealth.Health;
+        image.fillAmount = haveHealth.Health / 100;
     }
 
     private void HaveHealth_OnHealthChanged(object sender, System.EventArgs e)
-    {
-        slider.value = haveHealth.Health;
+    {        
+        image.fillAmount = haveHealth.Health / 100;
     }
 }
