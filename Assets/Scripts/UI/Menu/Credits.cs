1052e8b46c570535ee90efa9ab457c5cf198a3a2
Haley Phillips <haleyphillips928@gmail.com>
2020-05-12 17:24:38 -0500

    added game credits and how to play

---
+++ Assets/Scripts/UI/Menu/Credits.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+   public void ShowCredits()
+    {
+        Application.OpenURL("https://github.com/Rigamorphius/AfricanSky/wiki/External-Assets-Credits");
+    }
+}
