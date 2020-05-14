cdc087cac690b4a78705b38b38f7e91acf847dd7
Haley Phillips <haleyphillips928@gmail.com>
2020-05-14 15:53:30 -0500

    menu sfx

---
+++ Assets/Scripts/UI/Menu/MenuSFX.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MenuSFX : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    [SerializeField] AudioClip backSound;
+    [SerializeField] AudioClip hoverSound;
+    [SerializeField] AudioClip selectSound;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayBackSound()
+    {
+        audioSource.pitch = 1;
+        audioSource.PlayOneShot(backSound);
+    }
+
+    public void PlayHoverSound()
+    {
+        audioSource.pitch = Random.Range(0.5f, 1.5f);
+        audioSource.PlayOneShot(hoverSound);
+    }
+
+    public void PlaySelectSound()
+    {
+        audioSource.pitch = 1;
+        audioSource.PlayOneShot(selectSound);
+    }
+}
